e02a6fe71c084cde2359afbeb8e8188fcac2fb30
mobiwusihuan288 <mobiwusihuan288@163.com>
2022-04-09 19:34:04 +0800

    docker_boot项目

---
+++ src/main/java/com/mobini/docker/DockerBootApplication.java
@@ -0,0 +1,16 @@
+package com.mobini.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description: docker启动类
+ * @Author: Mobini
+ * @Date: 2022-04-09
+ */
+@SpringBootApplication
+public class DockerBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DockerBootApplication.class, args);
+    }
+}
---
+++ src/main/java/com/mobini/docker/controller/OrderController.java
@@ -0,0 +1,29 @@
+package com.mobini.docker.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @Description: Controller层
+ * @Author: Mobini
+ * @Date: 2022-04-09
+ */
+@RestController
+public class OrderController {
+    @Value("${server.port}")
+    private String port;
+
+    @RequestMapping("/order/docker")
+    public String helloDocker() {
+        return "hello docker" + "\t" + port + "\t" + UUID.randomUUID().toString();
+    }
+
+    @RequestMapping(value = "/order/index", method = RequestMethod.GET)
+    public String index() {
+        return "服务端口号: " + "\t" + port + "\t" + UUID.randomUUID().toString();
+    }
+}
